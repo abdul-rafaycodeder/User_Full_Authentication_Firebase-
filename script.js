ffe751bb3313d0d766d624c2d9d6abcc4630cfd1
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-02-28 14:44:37 -0800

    coding

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // ---------------------------==> All Imports <==------------------------------//
-import dotenv from "dotenv";
-dotenv.config();
-console.log(process.env.API_KEY)
+// import dotenv from "dotenv";
+// dotenv.config();
+// console.log(process.env.API_KEY)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: "AIzaSyAa5-hOZ4jbqU7KkK9m4-_ZIEKq8VmraHA",
     authDomain: "sir-ibrahim-website-clone.firebaseapp.com",
     projectId: "sir-ibrahim-website-clone",
     storageBucket: "sir-ibrahim-website-clone.firebasestorage.app",
@@ -20,8 +24,6 @@ const auth = getAuth(app);
 
 // ---------------------------==> Sign Up <==------------------------------//
 
-
-
 // btn add event "click"
 const btn = document.getElementById('btn');
 btn.addEventListener('click', signup);
@@ -34,7 +36,7 @@ function signup() {
     console.log('password1==>', password)
 
     // Firebase Code
-    createUserWithEmailAndPassword(auth,email, password)
+    createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
@@ -48,3 +50,6 @@ function signup() {
             // ..
         });
 }
+
+// ---------------------------==> Sign in <==------------------------------//
+
