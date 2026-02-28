6b1b684b1c15d48fde46f6f791df4e8d578a11a6
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-02-28 15:01:19 -0800

    coding

--- script.js
+++ script.js
@@ -66,11 +66,13 @@ function signin() {
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
+            console.log(user)
             // ...
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log(errorMessage)
         });
 
 }
